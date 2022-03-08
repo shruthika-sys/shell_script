087838ea736ca6b31f6aea3475af1df16a0cc96a
shruthika-sys <98949991+shruthika-sys@users.noreply.github.com>
2022-03-08 15:24:47 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -1,17 +1,16 @@
- set -- `who`
- u=$1
-
- set -- `echo $4 | tr ":" " "`
- h=$1
- if [ $h -ge 4 ] && [ $h -lt 12 ]
- then
- echo "Good morning Mr/Mrs. $u"
- elif [ $h -ge 12 ] && [ $h -lt 16 ] 
- then
- echo "Good afternoon Mr.Mrs. $u"
- elif [ $h -ge 16 ] && [ $h -lt 19 ]
- then
- echo "Good evening Mr.Mrs. $u"
- else
- echo "Good night Mr.Mrs. $u"
- fi
+#h=`date +%H`
+h=$1
+u=`whoami`
+echo " user logged in terminal at: $h"
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo -n "good morning $u"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo -n "Good afternoon $u"
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+echo -n "Good Evening $u"
+else
+echo -n "good night $u"
+fi
