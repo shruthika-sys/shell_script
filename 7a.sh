cb0c6bfa704fd81f4d66ede31bdaa60bbd3559a6
shruthika-sys <98949991+shruthika-sys@users.noreply.github.com>
2022-03-08 15:17:38 +0530

    Add files via upload

--- 7a.sh
+++ 7a.sh
@@ -1,20 +1,17 @@
-#Write a shell script that gets executed and displays the message either “Good Morning” or
-#“Good Afternoon” or “Good Evening” depending upon time at which the user logs in.
-set -- `who`
-g=$1
-t=$4
-set -- `echo $t | tr ":" " "`
-h=$1
-echo " user logged in terminal at: $h"
-if [ $h -ge 4 ] && [ $h -lt 12 ]
-then
-echo "GOOD MORNING mr. $g"
-elif [ $h -ge 12 ]  && [ $h -lt 16 ]
-then
-echo "GOOD afternoon mr.$g"
-elif [ $h -ge 16  ] && [ $h -lt 19 ]
-then
-echo "GOOD evening mr. $g"
-else
-echo "GOOD night mr. $g"
-fi
+ set -- `who`
+ u=$1
+
+ set -- `echo $4 | tr ":" " "`
+ h=$1
+ if [ $h -ge 4 ] && [ $h -lt 12 ]
+ then
+ echo "Good morning Mr/Mrs. $u"
+ elif [ $h -ge 12 ] && [ $h -lt 16 ] 
+ then
+ echo "Good afternoon Mr.Mrs. $u"
+ elif [ $h -ge 16 ] && [ $h -lt 19 ]
+ then
+ echo "Good evening Mr.Mrs. $u"
+ else
+ echo "Good night Mr.Mrs. $u"
+ fi
