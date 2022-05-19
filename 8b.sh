dd8160b1db25d8b91f67dee6ae234216eabcdb21
shruthika-sys <98949991+shruthika-sys@users.noreply.github.com>
2022-05-19 12:42:33 +0530

    Update 8b.sh

--- 8b.sh
+++ 8b.sh
@@ -1,3 +1,5 @@
+#Write a shell script that reports the logging on of as specified user within one minute after
+#he/she login. The script automatically terminates if specified user does not login during specified period of time.
 if [ $# -eq 1 ]
 then
 	var=`grep -owi $1 /etc/passwd`
